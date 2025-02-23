4f8388d49d5629cc3055f851736b8e7f3bcf351e
Julius <juliusarthur2005@gmail.com>
2025-02-24 01:38:48 +0700

    del: unused package

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 package Tucil1_13523030.src;
 import java.io.PrintWriter;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.Scanner;
 
 
 public class Main {
-    public static AtomicInteger  count = new AtomicInteger(0);
+    // public static AtomicInteger  count = new AtomicInteger(0);
     
     public static void main(String[] args) throws Exception{
-        count.set(0);
+        Count c = new Count();
         FileRead f;
         Game game;
         long duration;
@@ -28,17 +27,19 @@ public class Main {
 
         start_time = System.nanoTime();
 
-        boolean solved = game.solve_game(0);
+        boolean solved = game.solve_game(0, c);
         
         end_time = System.nanoTime();
         duration = end_time - start_time;
         String out = game.board.getBoard();
+
+        System.out.println("Waktu pencarian: " + duration/1000000 + " ms");
+        System.out.println("Banyak kasus yang ditinjau:  " + c.count);
         
         if (!(solved)){
             System.out.println("No possible solution");
         }
         else{
-            System.out.println("Solved");
             Scanner input2 = new Scanner(System.in);
             System.err.print("Apakah anda ingin menyimpan solusi? (ya/tidak)");
             String save = input2.nextLine();
@@ -54,8 +55,6 @@ public class Main {
             }
         }
         
-        System.out.println("Waktu pencarian: " + duration/1000000 + " ms");
-        System.out.println("Banyak kasus yang ditinjau:  " + count.get());
         
         
         
