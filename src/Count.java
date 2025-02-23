3a49de7b1bd5a0d19e3905bfac3c2db62f0ce379
Julius <juliusarthur2005@gmail.com>
2025-02-24 01:38:01 +0700

    feat: count class

---
+++ src/Count.java
@@ -0,0 +1,13 @@
+package Tucil1_13523030.src;
+
+public class Count {
+    public int count;
+    public Count(){
+        this.count = 0;
+    }
+
+    public void countIncr(){
+        this.count++;
+    }
+    
+}
